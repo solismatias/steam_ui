6f283e84a46f7d39aab2cbf498b320cbb4c31209
Matias Solis <solisma.dev@gmail.com>
2025-02-11 17:26:22 -0300

    refactor: use SteamPressable in SteamButton

--- lib/src/widgets/steam_button.dart
+++ lib/src/widgets/steam_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:steam_ui/src/theme.dart';
-import 'package:steam_ui/src/widgets/steam_container.dart';
+import 'package:steam_ui/src/widgets/widgets.dart';
 
 /// A customizable button widget with a pressed state that adjusts its styling,
 /// including the option to show or hide underlay text when disabled.
@@ -45,6 +45,12 @@ class SteamButton extends StatefulWidget {
 class _SteamButtonState extends State<SteamButton> {
   bool _pressed = false;
 
+  void _handleTap({required bool isPressed}) {
+    if (!widget._isDisabled) {
+      setState(() => _pressed = isPressed);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final steamTheme = Theme.of(context).extension<SteamTheme>();
@@ -53,40 +59,28 @@ class _SteamButtonState extends State<SteamButton> {
     // Use provided padding, or fall back to the theme's padding.
     final buttonPadding = widget.padding ?? buttonTheme!.padding;
 
-    void handleTap({required bool isPressed}) {
-      if (!widget._isDisabled) {
-        setState(() => _pressed = isPressed);
-      }
-    }
-
-    return MouseRegion(
-      child: GestureDetector(
-        onTapDown: (_) => handleTap(isPressed: true),
-        onTapUp: (_) => handleTap(isPressed: false),
-        onTapCancel: () => handleTap(isPressed: false),
-        onTap: widget._isDisabled ? null : widget.onPressed,
-        child: SteamContainer(
-          padding: buttonPadding,
-          alternateBorderColor: _pressed,
-          backgroundColor: buttonTheme?.backgroundColor,
-          child: Transform.translate(
-            offset: _pressed ? const Offset(1, 1) : Offset.zero,
-            child: Stack(
-              children: [
-                if (widget._isDisabled && widget.showUnderlayTextStyle)
-                  _UnderlayText(
-                    steamTheme: steamTheme,
-                    child: widget.child as Text,
-                  ),
-                _DefaultTextStyle(
-                  buttonTheme: buttonTheme,
-                  steamTheme: steamTheme,
-                  isDisabled: widget._isDisabled,
-                  child: widget.child,
-                ),
-              ],
+    return SteamPressable(
+      onPressed: () => widget.onPressed?.call(),
+      onPressStart: () => _handleTap(isPressed: true),
+      onPressEnd: () => _handleTap(isPressed: false),
+      child: SteamContainer(
+        padding: buttonPadding,
+        alternateBorderColor: _pressed,
+        backgroundColor: buttonTheme?.backgroundColor,
+        child: Stack(
+          children: [
+            if (widget._isDisabled && widget.showUnderlayTextStyle)
+              _UnderlayText(
+                steamTheme: steamTheme,
+                child: widget.child as Text,
+              ),
+            _DefaultTextStyle(
+              buttonTheme: buttonTheme,
+              steamTheme: steamTheme,
+              isDisabled: widget._isDisabled,
+              child: widget.child,
             ),
-          ),
+          ],
         ),
       ),
     );
