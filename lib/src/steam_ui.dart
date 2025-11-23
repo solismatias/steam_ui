573bce133c5799b513f098830e4b9a5617bad63b
solismatias <solismatias@users.noreply.github.com>
2025-11-23 20:22:06 -0300

    refactor: remove unused SteamUi class and associated tests

--- lib/src/steam_ui.dart
+++
@@ -1,5 +0,0 @@
-/// UI library inspired by the classic Steam interface from the early 2000s
-class SteamUi {
-  /// UI library inspired by the classic Steam interface from the early 2000s
-  const SteamUi();
-}
--- lib/steam_ui.dart
+++ lib/steam_ui.dart
@@ -1,6 +1,5 @@
 /// UI library inspired by the classic Steam interface from the early 2000s
 library;
 
-export 'src/steam_ui.dart';
 export 'src/theme.dart';
 export 'src/widgets/widgets.dart';
